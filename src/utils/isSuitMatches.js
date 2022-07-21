525533d730a6ff7f14daed16b2d5b318abf45ec4
RolandPi <roland@catalystvr.com.au>
2022-07-21 10:43:04 +0100

    fixed bug. stopped adding to matches due to incorrect if statment

--- src/utils/isSuitMatches.js
+++ src/utils/isSuitMatches.js
@@ -1,8 +1,7 @@
 const isSuitMatches = (leftcard, rightcard) => {
   if (typeof leftcard !== "object" || typeof rightcard !== "object") return false
   if (leftcard == null || rightcard == null) return false
-  if (leftcard !== null || rightcard !== null) return false
-  if (leftcard !== undefined || rightcard !== undefined) return false
+  if (leftcard.suit == undefined || rightcard.suit == undefined) return false
 
 
   if (leftcard.suit === rightcard.suit) {
--- src/utils/isValueMatches.js
+++ src/utils/isValueMatches.js
@@ -1,9 +1,7 @@
 const isValueMatches = (leftcard, rightcard) => {
   if (typeof leftcard !== "object" || typeof rightcard !== "object") return false
-  if (typeof leftcard !== "number" || typeof leftcard !== "number") return false
   if (leftcard == null || rightcard == null) return false
-  if (leftcard !== null || rightcard !== null) return false
-  if (leftcard !== undefined || rightcard !== undefined) return false
+  if (leftcard.value == null || rightcard.value == null) return false
 
   if (leftcard.value === rightcard.value) {
     return true;
